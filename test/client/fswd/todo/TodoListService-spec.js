89bc4bea936f33aad611c4fa6e2a3f79b98eddbc
David Raynes <rayners@gmail.com>
2016-10-26 20:55:29 -0400

    Example with non-promise return

--- test/client/fswd/todo/TodoListService-spec.js
+++ test/client/fswd/todo/TodoListService-spec.js
@@ -6,7 +6,7 @@ describe('fswd.todo.TodoListService', function() {
   beforeEach(angular.mock.module('fswd.todo'));
 
   afterEach(inject(function($httpBackend) {
-    $httpBackend.flush();
+    // $httpBackend.flush();
     $httpBackend.verifyNoOutstandingExpectation();
     $httpBackend.verifyNoOutstandingRequest();
   }));
@@ -14,9 +14,25 @@ describe('fswd.todo.TodoListService', function() {
   it('should retrieve a given task id', inject(function($httpBackend, TodoListService) {
     $httpBackend.expectGET('/tasks/14').respond(200, { id: 14, name: 'Tests!'});
 
-    return TodoListService.retrieveTodo(14)
+    var promise = TodoListService.retrieveTodo(14)
       .then(function(task) {
         task.should.deep.equal({ id: 14, name: 'Tests!'});
       });
+
+    $httpBackend.flush();
+    return promise;
+  }));
+
+  it('should add a new task', inject(function($httpBackend, TodoListService) {
+    $httpBackend.expectPOST('/tasks', { todo: 'beer' })
+      .respond(200, [ { id: 2, name: 'other task'}, { id: 3, name: 'beer '}]);
+
+      TodoListService.addTodo('beer');
+      $httpBackend.flush();
+
+      TodoListService.getTodoList().should.deep.equal([
+        { id: 2, name: 'other task'}, { id: 3, name: 'beer '}
+      ]);
+
   }));
 });
